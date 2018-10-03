ded813a11cab187af5a1a4f3e98af84c6a63a2e3
Cristian Contreras <crecontr@gmail.com>
2018-10-03 18:01:21 -0300

    Revert "v0.07"
    This reverts commit 0eaf569b610ecc824c0c6cf85aa41f83de0c72c8.

--- dideco/config/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$currentVersion = "1.0.0-beta";
-	
-	$target_dir_trabajos = "../../../hidrotecnica_img_trabajos/";
-	$target_dir_firmas = "../../../hidrotecnica_img_firmas/";
-
-?>
--- dideco/searchForm/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-  header("location: ./procedures/find_procedures.php");
-	
-?>
